e520ef342642ef9ab268cb1ac9a23943e0d2faaa
Enricoka12 <kettohallo@gmail.com>
2024-01-11 00:55:37 +0100

    Updated many files and projects

---
+++ app/Drum Kit/index.js
@@ -0,0 +1,66 @@
+var dobokSzama = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < dobokSzama; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+
+        var buttonInnerHtml = this.innerHTML;
+         var currentKey = buttonInnerHtml;
+         console.log(currentKey)
+
+
+        switch (buttonInnerHtml) {
+            case "w":
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+                break;
+
+
+            case "a":
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+                break;
+
+            case "s":
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+                break;
+
+
+            case "d":
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break;
+
+
+            case "k":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+                break;
+
+
+            case "l":
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();
+                break;
+
+
+            default:
+                break;
+        }
+
+
+    });
+
+}
+
+function buttonAnimation(currentKey) {
+
+var activeButton = document.querySelector("."+ currentKey);
+    activeButton.classList.add("pressed")
+}
+
