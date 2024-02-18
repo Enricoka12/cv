962902c1883eb539996154d161b7d441de0e72cf
Enricoka12 <kettohallo@gmail.com>
2024-02-18 23:24:27 +0100

    updated cv

--- script.js
+++ script.js
@@ -30,10 +30,33 @@ console.log(newdate)
           }
           
       }); 
-  
+
+
+      
   });
 
-  
+  $(window).scroll( function(){
+    
+    /* Check the location of each desired element */
+    $('.fade2').each( function(i){
+        
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > bottom_of_object-650 ){
+            
+            $(this).animate({'opacity':'1'},1200);
+            
+                
+        }
+        
+    }); 
+
+
+    
+});
+
 
 
 
@@ -54,3 +77,8 @@ console.log(newdate)
     // Megnyitjuk az e-mail alkalmazást az elkészített mailto linkkel
     window.location.href = mailtoLink;
 });
+
+
+
+
+
